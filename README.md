c89c0257c0f7075a389393f4076c4ef60670945b
Anders Riutta <git@andersriutta.com>
2015-12-21 12:39:30 -0800

    Further experimental work.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # rx-fs
+
+This is an [RxJs](https://github.com/Reactive-Extensions/RxJS) wrapper for the Node.js [fs module](https://nodejs.org/api/fs.html). It is intended to track the `fs` API as closely as possible, with the following exceptions:
+
+* If the `fs` method expects a callback, the `rx-fs` method instead returns an Observable
+* `Observable` replaces `Stream`
+
+Not all the methods are currently supported. Pull requests welcome!
+
+See also [this alternative](https://github.com/trxcllnt/rxjs-fs) that has a more full-featured API but does not match the current node fs API.
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var _ = require('lodash');
 var fs = require('fs');
 var Rx = require('rx');
-var RxNode = require('../rx-node-extras/index.js');
+var RxNode = require('rx-node-extra');
 
 // TODO not currently handling 'this' and prototypes
 function rxifyNodeObject(inputObject) {
@@ -38,20 +38,18 @@ var rxFs = rxifyNodeObject(fs);
 rxFs.createReadObservable = function(path, options) {
   var stream = fs.createReadStream(path, options);
   options = options || {};
-  return RxNode.fromReadableStream(stream);
+
+  var openSource = Rx.Observable.fromEvent(stream, 'open');
+  var resultSource = RxNode.fromReadableStream(stream);
+  resultSource.openSource = openSource;
+  return resultSource;
 };
 
 rxFs.createWriteObservable = function(path, options) {
-  var writeStream = fs.createWriteStream(path, options);
-
-  return function(x, idx, source) {
-    var output;
-    if (_.isPlainObject(x) || _.isArray(x)) {
-      output = JSON.stringify(x, null, '  ');
-    } else {
-      output = x;
-    }
-    writeStream.write(output);
+  var stream = fs.createWriteStream(path, options);
+  return function(source) {
+    RxNode.writeToStream(source, stream);
+    return source;
   };
 };
 
